4f24a8de9a1b6ad2cf847a441d66c23fd957fa4b
Henry Don <nguyendonghung70@gmail.com>
2026-08-14 02:11:27 +0700

    Remove unused adjacent question loader

--- scripts/exam-generator/module-c-extra.mjs
+++
@@ -1,9 +0,0 @@
-import { dataFactories } from './module-c-extra-data.mjs';
-import { ragFactories } from './module-c-extra-rag.mjs';
-import { monitoringFactories } from './module-c-extra-monitoring.mjs';
-
-export const extraCFactories = {
-  ...dataFactories,
-  ...ragFactories,
-  ...monitoringFactories
-};
