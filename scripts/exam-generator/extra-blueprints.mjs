e5b4fdf94b576a2769c357d9841c63add4507840
Henry Don <nguyendonghung70@gmail.com>
2026-08-14 02:00:45 +0700

    Load three additional exam blueprints

--- scripts/exam-generator/extra-blueprints.mjs
+++ scripts/exam-generator/extra-blueprints.mjs
@@ -1,1 +1,5 @@
-export const EXTRA_EXAM_BLUEPRINTS = [];
+import { EXAM_11 } from './extra-blueprint-11.mjs';
+import { EXAM_12 } from './extra-blueprint-12.mjs';
+import { EXAM_13 } from './extra-blueprint-13.mjs';
+
+export const EXTRA_EXAM_BLUEPRINTS = [EXAM_11, EXAM_12, EXAM_13];
